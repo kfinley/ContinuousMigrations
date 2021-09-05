e2afad12aee2842d5727d5dd958028e8db9573a1
kfinley <kyle@kylefinley.net>
2021-09-04 17:33:33 -0700

    Versions & Nuget: Added EF3 and EF5 support. Added GitHub Actions to publish Nuget packages.

--- src/Extensions.cs
+++ src/Extensions.cs
@@ -6,37 +6,37 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace ContinuousMigrations {
-    public static class Extensions {
+  public static class Extensions {
 
-        private static void ConfigureSettings(this IServiceCollection services) {
+    private static void ConfigureSettings(this IServiceCollection services) {
 
-            string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+      string env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json")
-                .AddJsonFile($"appsettings.{env}.json", optional: true)
-                .Build();
+      var config = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .AddJsonFile($"appsettings.{env}.json", optional: true)
+          .Build();
 
-            services.Configure<Settings>(s => config.GetSection("ContinuousMigrations").Bind(s));
-        }
-
-        public static IServiceCollection AddContinuousMigrations<T>(this IServiceCollection services)
-            where T : DbContext {
-
-            services.ConfigureSettings();
-
-            using (var scope = services.BuildServiceProvider().CreateScope()) {
-
-                var settings = scope.ServiceProvider.GetRequiredService<IOptions<Settings>>();
-
-                if (settings.Value.Enabled)
-                    using (var context = scope.ServiceProvider.GetRequiredService<T>())
-                        context.Database.Migrate();
-
-            }
-
-            return services;
-        }
+      services.Configure<Settings>(s => config.GetSection("ContinuousMigrations"));
     }
+
+    public static IServiceCollection AddContinuousMigrations<T>(this IServiceCollection services)
+        where T : DbContext {
+
+      services.ConfigureSettings();
+
+      using (var scope = services.BuildServiceProvider().CreateScope()) {
+
+        var settings = scope.ServiceProvider.GetRequiredService<IOptions<Settings>>();
+
+        if (settings.Value.Enabled)
+          using (var context = scope.ServiceProvider.GetRequiredService<T>())
+            context.Database.Migrate();
+
+      }
+
+      return services;
+    }
+  }
 }
